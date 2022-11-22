f91aca34754ecbb80b74dd3894c46b176081d8ce
Vutlhari Doctor Mashimbyi <88923436+MashKeyZ@users.noreply.github.com>
2022-11-22 16:16:27 +0200

    Update DocumentScannerModule.java

--- android/src/main/java/com/documentscanner/DocumentScannerModule.java
+++ android/src/main/java/com/documentscanner/DocumentScannerModule.java
@@ -4,6 +4,7 @@ import com.documentscanner.views.MainView;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
 
 import android.graphics.Bitmap;
 import android.net.Uri;
